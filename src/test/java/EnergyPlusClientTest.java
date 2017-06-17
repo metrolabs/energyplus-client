4d5fd3e1d32b5c4476f610139e68a533ffaf8b32
wgayton <wgayton@gmail.com>
2017-06-17 12:30:53 -0400

    Day hours are now 0-23 instead of 1-24

--- src/test/java/EnergyPlusClientTest.java
+++ src/test/java/EnergyPlusClientTest.java
@@ -19,14 +19,12 @@
  */
 
 
-import com.google.gson.Gson;
 import org.energyplus.EnergyPlusClient;
 import org.energyplus.WeatherData;
 import org.junit.Test;
 
 import java.util.TimeZone;
 
-import static org.energyplus.EnergyPlusClient.Constants.HOURS_OF_MONTH_ACC;
 import static org.junit.Assert.assertEquals;
 
 public class EnergyPlusClientTest {
@@ -41,11 +39,6 @@ public class EnergyPlusClientTest {
 
         String url = "https://energyplus.net/weather-download/north_and_central_america_wmo_region_4/USA/NM/USA_NM_Albuquerque.Intl.AP.723650_TMY3/USA_NM_Albuquerque.Intl.AP.723650_TMY3.epw";
         WeatherData weatherData = EnergyPlusClient.toWeatherDataFromLink(url);
-
-        System.out.println(new Gson().toJson(weatherData.getWeatherRawData().get(0)));
-        System.out.println(new Gson().toJson(weatherData.getWeatherRawData().get(HOURS_OF_MONTH_ACC[8]+12)));
-        System.out.println(new Gson().toJson(weatherData.getWeatherRawData().get(8759)));
-
         assertEquals(EnergyPlusClient.toWeatherDataFromLink(url).getWeatherRawData().size(), 8760);
     }
 }
